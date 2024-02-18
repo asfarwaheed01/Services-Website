cf72ee36317e81b1ae3c4eeafd3fda6d9f6de8c8
asfarwaheed01 <asfar1024962@gmail.com>
2024-02-18 20:17:15 +0500

    height

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -55,7 +55,7 @@ const Projects = () => {
               <h2>DAMAC Hills Villas Renovation</h2>
             </div>
           </div>
-          <div className="project-container md:w-[30%] w-[90%] mx-auto md:h-[600px] h-[300px] rounded-lg overflow-hidden relative">
+          <div className="project-container md:w-[30%] w-[90%] mx-auto md:h-[600px] h-[350px] rounded-lg overflow-hidden relative">
             <img
               src={p3}
               alt=""
